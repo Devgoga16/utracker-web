fb6e8fdf26a04f4e77ba4cb07711c69ca74503a8
Devgoga16 <mgdammy15@gmail.com>
2026-08-15 01:35:35 -0500

    cambios2

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://utracker-api.unify-tc.com',
         changeOrigin: true,
       },
     },
